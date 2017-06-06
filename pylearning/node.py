513a4639019f12f4c8c164c3bf30cc52fe29597e
amstut_a <arthur.amstutz@epitech.eu>
2017-06-06 19:12:02 +0100

    commented Node class

--- pylearning/node.py
+++ pylearning/node.py
@@ -1,4 +1,13 @@
 class DecisionNode:
+    """
+    Represents a node / leaf in a tree.
+    :param col:     Column of the variable to split in this node
+    :param value:   Value of the variable used to split
+    :param result:  Result contained in leaf nodes
+    :param tb:      Left branch of the split
+    :param fb:      Right branch of the split
+    """
+
     def __init__(self, col=-1, value=None, result=None, tb=None, fb=None):
         self.col = col
         self.value = value
