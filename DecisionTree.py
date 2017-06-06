0852456d38ecfbbf199660c7e01155ca52ee3ae4
amstut_a <arthur.amstutz@epitech.eu>
2017-06-06 12:15:20 +0100

    changed decision tree to predict continuous variables
    changed test to evaluate tree performance
    
    added simple function to read data file

--- DecisionTree.py
+++ DecisionTree.py
@@ -1,20 +1,10 @@
 #!/usr/bin/env python3
 
-import CSVReader
 import random
 from math import log, sqrt
 
 
-class DecisionTreeClassifier:
-
-    class DecisionNode:
-        def __init__(self, col=-1, value=None, results=None, tb=None, fb=None):
-            self.col = col
-            self.value = value
-            self.results = results
-            self.tb = tb
-            self.fb = fb
-
+class DecisionTreeRegressor:
     """
     :param  max_depth:          Maximum number of splits during training
     :param  random_features:    If False, all the features will be used to
@@ -23,31 +13,37 @@ class DecisionTreeClassifier:
                                 features.
                                 Usually, this option is used in a random
                                 forest.
+    :param min_leaf_examples:   Minimum number of examples in a meaf node.
     """
-    def __init__(self, max_depth=-1, random_features=False):
+
+    class DecisionNode:
+        def __init__(self, col=-1, value=None, result=None, tb=None, fb=None):
+            self.col = col
+            self.value = value
+            self.result = result
+            self.tb = tb
+            self.fb = fb
+
+
+    def __init__(self, max_depth=-1, random_features=False, min_leaf_examples=6):
         self.root_node = None
         self.max_depth = max_depth
         self.features_indexes = []
         self.random_features = random_features
+        self.min_leaf_examples = min_leaf_examples
 
     """
     :param  rows:       The data used to rain the decision tree. It must be a
                         list of lists. The last vaue of each inner list is the
                         value to predict.
-    :param  criterion:  The function used to split data at each node of the
-                        tree. If None, the criterion used is entropy.
     """
-    def fit(self, rows, criterion=None):
-        if len(rows) < 1:
+    def fit(self, features, targets):
+        if len(features) < 1:
             raise ValueError("Not enough samples in the given dataset")
-
-        if not criterion:
-            criterion = self.entropy
         if self.random_features:
-            self.features_indexes = self.choose_random_features(rows[0])
-            rows = [self.get_features_subset(row) + [row[-1]] for row in rows]
-
-        self.root_node = self.build_tree(rows, criterion, self.max_depth)
+            self.features_indexes = self.choose_random_features(features[0])
+            features = [self.get_features_subset(row) for row in features]
+        self.root_node = self.build_tree(features, targets, self.max_depth)
 
     """
     Returns a prediction for the given features.
@@ -82,45 +78,33 @@ class DecisionTreeClassifier:
     :param  column: The index of the column used to split data
     :param  value:  The value used for the split
     """
-    def divide_set(self, rows, column, value):
+    def divide_set(self, features, targets, column, feature_value):
         split_function = None
-        if isinstance(value, int) or isinstance(value, float):
-            split_function = lambda row: row[column] >= value
+        if isinstance(feature_value, int) or isinstance(feature_value, float):
+            split_function = lambda row: row[column] >= feature_value
         else:
-            split_function = lambda row: row[column] == value
+            split_function = lambda row: row[column] == feature_value
 
-        set1 = [row for row in rows if split_function(row)]
-        set2 = [row for row in rows if not split_function(row)]
+        set1 = [row for row in zip(features, targets) if split_function(row[0])]
+        set2 = [row for row in zip(features, targets) if not split_function(row[0])]
 
-        return set1, set2
+        feat1, targs1 = [x[0] for x in set1], [x[1] for x in set1]
+        feat2, targs2 = [x[0] for x in set2], [x[1] for x in set2]
 
-    """
-    Returns the occurence of each result in the given dataset.
-    :param  rows:   A list of lists with the output at the last index of
-                    each one
-    """
-    def unique_counts(self, rows):
-        results = {}
-        for row in rows:
-            r = row[len(row) - 1]
-            if r not in results:
-                results[r] = 0
-            results[r] += 1
-        return results
+        return feat1, targs1, feat2, targs2
 
-    """
-    Returns the entropy in the given rows.
-    :param  rows:   A list of lists with the output at the last index of
-                    each one
-    """
-    def entropy(self, rows):
-        log2 = lambda x: log(x) / log(2)
-        results = self.unique_counts(rows)
-        ent = 0.0
-        for r in results.keys():
-            p = float(results[r]) / len(rows)
-            ent = ent - p * log2(p)
-        return ent
+
+    def mean_output(self, targets):
+        return sum(targets) / len(targets)
+
+
+    def variance(self, targets):
+        if len(targets) == 0:
+            return None
+        mean = self.mean_output(targets)
+        variance = sum([(x - mean)**2 for x in targets])
+        return variance
+
 
     """
     Recursively creates the decision tree by splitting the dataset until no
@@ -130,40 +114,46 @@ class DecisionTreeClassifier:
                     condition
     :param  depth:  The current depth in the tree
     """
-    def build_tree(self, rows, func, depth):
-        if len(rows) == 0:
+    def build_tree(self, features, targets, depth):
+        if len(features) == 0:
             return self.DecisionNode()
         if depth == 0:
-            return self.DecisionNode(results=self.unique_counts(rows))
+            return self.DecisionNode(result=self.mean_output(targets))
+            # Add check: if all features have same value, stop
 
-        current_score = func(rows)
-        best_gain = 0.0
+        lowest_variance = None
         best_criteria = None
         best_sets = None
-        column_count = len(rows[0]) - 1
 
-        for col in range(0, column_count):
-            column_values = {}
-            for row in rows:
-                column_values[row[col]] = 1
-            for value in column_values.keys():
-                set1, set2 = self.divide_set(rows, col, value)
+        for column in range(len(features[0])):
+            column_values = sorted([feature[column] for feature in features]) # Maybe remove sorted
+            for feature_value in column_values:
+                feats1, targs1, feats2, targs2 = \
+                    self.divide_set(features, targets, column, feature_value)
+                var1 = self.variance(targs1) # Replace variance by deviation
+                var2 = self.variance(targs2)
+                if var1 is None or var2 is None:
+                    continue
+                variance = var1 + var2
 
-                p = float(len(set1)) / len(rows)
-                gain = current_score - p * func(set1) - (1 - p) * func(set2)
-                if gain > best_gain and len(set1) > 0 and len(set2) > 0:
-                    best_gain = gain
-                    best_criteria = (col, value)
-                    best_sets = (set1, set2)
+                if lowest_variance is None or variance < lowest_variance:
+                    lowest_variance = variance
+                    best_criteria   = (column, feature_value)
+                    best_sets       = ((feats1, targs1),(feats2, targs2))
 
-        if best_gain > 0:
-            trueBranch = self.build_tree(best_sets[0], func, depth - 1)
-            falseBranch = self.build_tree(best_sets[1], func, depth - 1)
+        # Check variance value also
+        if lowest_variance is not None and \
+            len(best_sets[0][0]) >= self.min_leaf_examples and \
+            len(best_sets[1][0]) >= self.min_leaf_examples:
+            left_branch = self.build_tree(best_sets[0][0], best_sets[0][1], depth - 1)
+            right_branch = self.build_tree(best_sets[1][0], best_sets[1][1], depth - 1)
             return self.DecisionNode(col=best_criteria[0],
                                      value=best_criteria[1],
-                                     tb=trueBranch, fb=falseBranch)
+                                     tb=left_branch,
+                                     fb=right_branch)
         else:
-            return self.DecisionNode(results=self.unique_counts(rows))
+            return self.DecisionNode(result=self.mean_output(targets))
+
 
     """
     Makes a prediction using the given features.
@@ -171,8 +161,8 @@ class DecisionTreeClassifier:
     :param  tree:           The current node
     """
     def classify(self, observation, tree):
-        if tree.results is not None:
-            return list(tree.results.keys())[0]
+        if tree.result is not None:
+            return tree.result
         else:
             v = observation[tree.col]
             branch = None
@@ -189,14 +179,40 @@ class DecisionTreeClassifier:
             return self.classify(observation, branch)
 
 
-def test_tree():
-    data = CSVReader.read_csv("../data/income.csv")
-    tree = DecisionTreeClassifier(random_features=True)
-    tree.fit(data)
 
-    print(tree.predict([39, 'State-gov', 'Bachelors', 13, 'Never-married',
-                        'Adm-clerical', 'Not-in-family', 'White', 'Male',
-                        2174, 0, 40, 'United-States']))
+
+def read_data(filepath):
+    features, targets = [], []
+    with open(filepath) as fd:
+        for row in fd:
+            row = row.split()
+            features.append([float(row[x]) for x in range(21)])
+            targets.append(float(row[21]))
+    return features, targets
+
+
+def test_tree():
+    from sklearn.model_selection import train_test_split
+
+    features, targets = read_data("user_cpu.data")
+
+    train, test = train_test_split(list(zip(features, targets)), test_size=0.3)
+    train_features = [f[0] for f in train]
+    train_targets = [t[1] for t in train]
+
+    tree = DecisionTreeRegressor(random_features=True)
+    tree.fit(train_features, train_targets)
+
+    absolute_errors = []
+    squared_errors = []
+    for feat, targ in test:
+        pred = tree.predict(feat)
+        squared_errors.append((pred - targ)**2)
+        absolute_errors.append(abs(pred - targ))
+
+    mae = sum(absolute_errors) / len(absolute_errors)
+    rmse = sqrt(sum(squared_errors) / len(squared_errors))
+    print("MAE = {}; RMSE = {}".format(mae, rmse))
 
 
 if __name__ == '__main__':
