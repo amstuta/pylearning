c5742269bb5194b87bdc249b2234f276a8d4c14a
amstut_a <arthur.amstutz@epitech.eu>
2017-06-06 13:07:47 +0100

    RandomForest now works with continous targets
    added example function for random forest

--- RandomForest.py
+++ RandomForest.py
@@ -3,12 +3,10 @@
 import logging
 import random
 from concurrent.futures import ProcessPoolExecutor
-
-import CSVReader
-from DecisionTree import DecisionTreeClassifier
+from DecisionTree import DecisionTreeRegressor
 
 
-class RandomForestClassifier(object):
+class RandomForestRegressor:
 
     """
     :param  nb_trees:       Number of decision trees to use
@@ -28,11 +26,12 @@ class RandomForestClassifier(object):
     :param  data:   A list of lists with the last element of each list being
                     the value to predict
     """
-    def fit(self, data):
+    def fit(self, features, targets):
         with ProcessPoolExecutor(max_workers=self.max_workers) as executor:
-            rand_fts = map(lambda x: [x, random.sample(data, self.nb_samples)],
-                           range(self.nb_trees))
-            self.trees = list(executor.map(self.train_tree, rand_fts))
+            zipped = list(zip(features, targets))
+            random_features = \
+                [(x, random.sample(zipped, self.nb_samples)) for x in range(self.nb_trees)]
+            self.trees = list(executor.map(self.train_tree, random_features))
 
     """
     Trains a single tree and returns it.
@@ -41,8 +40,9 @@ class RandomForestClassifier(object):
     """
     def train_tree(self, data):
         logging.info('Training tree {}'.format(data[0] + 1))
-        tree = DecisionTreeClassifier(max_depth=self.max_depth)
-        tree.fit(data[1])
+        tree = DecisionTreeRegressor(max_depth=self.max_depth)
+        features, targets = [x[0] for x in data[1]], [x[1] for x in data[1]]
+        tree.fit(features, targets)
         return tree
 
     """
@@ -51,33 +51,40 @@ class RandomForestClassifier(object):
     :param  feature:    The features used to predict
     """
     def predict(self, feature):
-        predictions = []
+        predictions = [tree.predict(feature) for tree in self.trees]
+        ensemble_prediction = sum(predictions) / len(predictions)
+        return ensemble_prediction
 
-        for tree in self.trees:
-            predictions.append(tree.predict(feature))
 
-        return max(set(predictions), key=predictions.count)
 
 
 def test_rf():
+    from math import sqrt
     from sklearn.model_selection import train_test_split
 
-    data = CSVReader.read_csv("../data/income.csv")
-    train, test = train_test_split(data, test_size=0.3)
+    features, targets = [], []
+    with open("user_cpu.data") as fd:
+        for row in fd:
+            row = row.split()
+            features.append([float(row[x]) for x in range(21)])
+            targets.append(float(row[21]))
+    train, test = train_test_split(list(zip(features, targets)), test_size=0.3)
+    train_features = [f[0] for f in train]
+    train_targets = [t[1] for t in train]
 
-    rf = RandomForestClassifier(nb_trees=60, nb_samples=3000, max_workers=4)
-    rf.fit(train)
+    rf = RandomForestRegressor(nb_trees=10, nb_samples=1000, max_workers=4)
+    rf.fit(train_features, train_targets)
 
-    errors = 0
-    features = [ft[:-1] for ft in test]
-    values = [ft[-1] for ft in test]
+    absolute_errors = []
+    squared_errors = []
+    for feat, targ in test:
+        pred = rf.predict(feat)
+        squared_errors.append((pred - targ)**2)
+        absolute_errors.append(abs(pred - targ))
+    mae = sum(absolute_errors) / len(absolute_errors)
+    rmse = sqrt(sum(squared_errors) / len(squared_errors))
+    print("MAE = {}; RMSE = {}".format(mae, rmse))
 
-    for feature, value in zip(features, values):
-        prediction = rf.predict(feature)
-        if prediction != value:
-            errors += 1
-
-    logging.info("Error rate: {}".format(errors / len(features) * 100))
 
 
 if __name__ == '__main__':
