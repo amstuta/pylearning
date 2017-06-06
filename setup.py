91a1440ea6a228ec9e4dca3f66ddb33ecd0ac9ba
amstut_a <arthur.amstutz@epitech.eu>
2017-06-06 19:47:44 +0100

    added setup.py file

---
+++ setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as fd:
+    long_description = fd.read()
+
+setup(
+    name='pylearning',
+
+    version='1.0.0b1',
+
+    description='Simple high-level library to use decision trees and random forest learners',
+    long_description=long_description,
+
+    url="https://github.com/amstuta/pylearning.git",
+
+    author="Arthur Amstutz",
+    author_email="arthur.amstutz@gmail.com",
+
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Machine Learning',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5'
+    ],
+
+    keywords='machine learning data decision trees random forest',
+
+    packages=find_packages(exclude=['contrib','docs','tests']),
+
+    # install_requires=[],
+    # extras_require={},
+    # package_data={},
+)
